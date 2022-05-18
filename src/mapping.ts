660e6514d597f40ceb2442ca0383c017e8441642
saman <saman_waruka@cmu.ac.th>
2022-05-18 11:51:00 +0700

    fix: cannot compile

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,4 +1,4 @@
-import { BigInt } from "@graphprotocol/graph-ts";
+import { BigInt, store } from "@graphprotocol/graph-ts";
 import {
   TodoList,
   TaskCreated,
@@ -8,14 +8,21 @@ import {
 } from "../generated/TodoList/TodoList";
 import { Task, Owner } from "../generated/schema";
 
+// function getTaskId(
+//   event: TaskCreated | TaskDeleted | TaskEdited | ToggleTaskCompleted
+// ): string {
+//   const ownerAddress = event.transaction.from.toHexString();
+//   const paramId = event.params.id.toString();
+//   return `${ownerAddress}_${paramId}`;
+// }
+
 function getTaskId(ownerAddress: string, paramId: string): string {
   return `${ownerAddress}_${paramId}`;
 }
 
 export function handleTaskCreated(event: TaskCreated): void {
   const transactionFrom = event.transaction.from.toHexString();
-  const eventParamId = event.params.id.toString();
-  const taskId = getTaskId(transactionFrom, eventParamId);
+  const taskId = getTaskId(transactionFrom, event.params.id.toString());
   let task = Task.load(taskId);
   if (!task) {
     task = new Task(taskId);
@@ -28,26 +35,38 @@ export function handleTaskCreated(event: TaskCreated): void {
   let owner = Owner.load(transactionFrom);
   if (!owner) {
     owner = new Owner(transactionFrom);
+    owner.save();
   }
-  owner.save();
 }
 
-// export function handleTaskDeleted(event: TaskDeleted): void {
-//   const entity = Task.load(event.params.id.toString());
-// }
+export function handleTaskDeleted(event: TaskDeleted): void {
+  const taskId = getTaskId(
+    event.transaction.from.toHexString(),
+    event.params.id.toString()
+  );
+  store.remove("Task", taskId);
+}
 
-// export function handleTaskEdited(event: TaskEdited): void {
-//   const entity = Task.load(event.params.id.toString());
-//   if (entity) {
-//     entity.content = event.params.content;
-//     entity.save();
-//   }
-// }
+export function handleTaskEdited(event: TaskEdited): void {
+  const taskId = getTaskId(
+    event.transaction.from.toHexString(),
+    event.params.id.toString()
+  );
+  const task = Task.load(taskId);
+  if (task) {
+    task.content = event.params.content;
+    task.save();
+  }
+}
 
-// export function handleToggleTaskCompleted(event: ToggleTaskCompleted): void {
-//   const entity = Task.load(event.params.id.toString());
-//   if (!entity) {
-//     return;
-//   }
-//   entity.completed = entity.completed;
-// }
+export function handleToggleTaskCompleted(event: ToggleTaskCompleted): void {
+  const taskId = getTaskId(
+    event.transaction.from.toHexString(),
+    event.params.id.toString()
+  );
+  const task = Task.load(taskId);
+  if (!task) {
+    return;
+  }
+  task.completed = task.completed;
+}
